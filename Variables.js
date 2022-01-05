5aca74ce48c34bba6c758e2fd6f2b3b1e2cade9a
kr-nirmit <nirmit.t@knackroot.com>
2022-01-05 11:06:06 +0530

    add variables.js file

---
+++ Variables.js
@@ -0,0 +1,27 @@
+// var name = "Nirmit";
+// name = "Thummar";
+// console.log(name);
+
+// let name1 = "Nirmit";
+// name1 = "Thummar";
+// console.log(name1);
+
+// const name2 = "Nirmit";
+// name2 = "Thummar";
+// console.log(name2);
+
+// function nodeJs(node) {
+//     if (node) {
+//         let name = "nirmit";
+//         console.log(name);
+//     }
+// }
+// nodeJs(true);
+
+function nodeJs(node) {
+    if (node) {
+        var name = "nirmit";
+    }
+    console.log(name);
+}
+nodeJs(true);
